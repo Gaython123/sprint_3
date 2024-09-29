9aa12eb47da686972c3c327484ca82616986fbdf
lekorda <lesik2709@gmail.com>
2024-09-29 15:10:58 +0200

    6.done

--- var 6.py
+++ var 6.py
@@ -1,14 +1,56 @@
-from datetime import datetime
 
+Days = []
 class Day:
-    def __init__(self, date):
-        self.date = date
-        #date = datetime.uptime(date, '%d/%m/%Y')
+    def __init__(self, day: int, month: int, year: int):
+        self.day = day
+        self.month = month
+        self.year = year
 
     def display(self):
-        print(self.date)
+        print(f"Date is day: {self.day}/{self.month}/{self.year}")
 
-day2 = Day(datetime.today())
-day1 = Day("28/09/2024")
-day1.display()
+    def add_day(self):
+        Days.append(self)
+
+    def days_to_end_of_month(self):
+        if self.month == 2 and self.year % 4 == 0 or self.year % 100 == 0 and self.year % 400 == 0:
+            if self.day != 29:
+                print(f"To the end of month are '{29 - self.day}' days left\n")
+            else:
+                print("It's the last day of month\n")
+
+        elif self.month == 2:
+            if self.day != 28:
+                print(f"To the end of month are '{28 - self.day}' days left\n")
+            else:
+                print("It's the last day of month\n")
+
+        elif self.month == 1 or self.month == 3 or self.month == 5 or self.month == 7 or self.month == 8 or self.month == 10 or self.month == 12:
+            if self.day != 31:
+                print(f"To the end of month are '{31 - self.day}' days left\n")
+            else:
+                print("It's the last day of month\n")
+                
+        else:
+            if self.day != 30:
+                print(f"To the end of month are '{30 - self.day}' days left\n")
+            else:
+                print("It's the last day of month\n")
+
+day2 = Day(12,8, 2024)
 day2.display()
+day2.days_to_end_of_month()
+
+day3 = Day(1, 1, 2024)
+day3.display()
+day3.days_to_end_of_month()
+
+day4 = Day(29, 2, 2024)
+day4.display()
+day4.days_to_end_of_month()
+
+
+
+
+
+
