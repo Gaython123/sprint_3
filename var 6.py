96de6d6c2a968d757883d436d51497390f6ddf64
lekorda <lesik2709@gmail.com>
2024-10-02 09:15:29 +0200

    modified 2

--- var 6.py
+++ var 6.py
@@ -13,74 +13,84 @@ class Day:
     def display(self):
         print(f"Date is day: {self.day}/{self.month}/{self.year}")
 
+#поняття оної відповідальності / single responsibility
     def days_to_end_of_month(self):
         if self.month == 2 and self.year % 4 == 0 or self.year % 100 == 0 and self.year % 400 == 0:
             if self.day != 29:
-                print(f"To the end of month are '{29 - self.day}' days left\n")
+                return 29 - self.day
             else:
-                print("It's the last day of month\n")
+                return 0
 
         elif self.month == 2:
             if self.day != 28:
-                print(f"To the end of month are '{28 - self.day}' days left\n")
+                return 28 - self.day
             else:
-                print("It's the last day of month\n")
+                return 0
 
         elif self.month == 1 or self.month == 3 or self.month == 5 or self.month == 7 or self.month == 8 or self.month == 10 or self.month == 12:
             if self.day != 31:
-                print(f"To the end of month are '{31 - self.day}' days left\n")
+                return 31 - self.day
             else:
-                print("It's the last day of month\n")
+                return 0
                 
         else:
             if self.day != 30:
-                print(f"To the end of month are '{30 - self.day}' days left\n")
+                return 30 - self.day
             else:
-                print("It's the last day of month\n")
+                return 0
+    def print_days_to_the_end_of_month(self):
+        print(f"Days to the end of month : {self.days_to_end_of_month()}")
 
-    def find_by_day(self):
-        for values in plans:
-            if Day in plans:
-                plans[Planner] = Day
-                print(f"for {Day} you have {Planner}")
-
-class Planner:
-    def __init__(self, task: str):
+class Task:
+    def __init__(self, task: str, action):
         self.task = task
+        self.action = action
 
     def __str__(self):
         return f"{self.task}"
 
+    def do_action(self, action):
+        return self.action()
+
+class Planner:
+    def __init__(self):
+        self.plans = {}
+#Тут я працюю з класами тому прописую все в класы)
+
+    def __str__(self):
+        return f"{self.plans}"
+
     def display(self):
-        print(f"The plan is '{self.task}'")
+        print(f"The plan is '{self.plans}'")
 
-    def add_a_day(Planner, Day):
-        new_plan = Planner
-        new_day = Day
-        plans[new_plan] = [new_day]
-        print(f"You have planned '{new_plan}' to day '{new_day}'")
+    def add_task(self, task: Task, day: Day):
+        self.plans[task] = day
+    #def calculate_/ add_/ - return
+    #view_/ show_/ = print
 
+    def view_plan(self):
+        for task in self.plans:
+            print(f"{task} : {self.plans[task]} description: {task.action()}")
 
-    def change_plan(self):
+    #def view_planner
 
-        old_plan = Planner
-        old_date = Day
-        plans = {old_plan: old_date}
-
-        #for old_date in plans:
-        new_plan = Planner(input("Enter new plan"))
-        old_plan = new_plan
-        print(plans)
-
-            #plans = {new_plan.__str__() : old_date}
+planner = Planner()
+#lambda - скорочена верся функції якв прописуэться у один рядок
+#у лямбді ретурн не пишем. Ретурн є неявний
 
 
-wash_mother = Planner("wash penis")
-first_plan = Day(12,9,2025)
+task = Task("Play Games", lambda: "We are playing games!")
+day = Day(1, 12, 2024)
+planner.add_task(task, day)
+#planner.view_plan()
 
-wash_mother.add_a_day(first_plan)
-wash_mother.change_plan()
-wash_mother.change_plan()
+new_task = Task("Have breakfast", lambda: "Bon apettito")
+new_day = Day(12,12,12)
+planner.add_task(new_task, new_day)
+planner.view_plan()
+#Коли мми пишемо () ми хочемо викликати функцію а коли хочемо передати -
+# РЕТУРН то без дужок
+
 
 
 
