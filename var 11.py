910fdc3d5084fd3427ec868fdd1c1bc21c049830
lekorda <lesik2709@gmail.com>
2024-10-11 13:41:59 +0200

    modified 11.1 but instead of writing 6 coordinates, created class Point with (2 coordinates in it)

--- var 11.py
+++ var 11.py
@@ -1,11 +1,14 @@
 class Point:
     def __init__(self, coordinate_1: int, coordinate_2:int):
+        """
+        :param coordinate_1: first coordinate of certain point
+        :param coordinate_2: second coordinate of certain point
+        """
         self.coordinate_1 = coordinate_1
         self.coordinate_2 = coordinate_2
 
     def __str__(self):
         return f"{self.coordinate_1};{self.coordinate_2}"
-
 
 class Triangle:
     def __init__(self, name, point_1: Point, point_2: Point, point_3: Point):
@@ -37,40 +40,48 @@ class Triangle:
               f"C({self.point_3})")
 
 
+    def square_of_triangle(self):
+        """
+        :return: Square of triangle by its coordinates
 
-    #def square_of_triangle(self):
-    #    """
-    #    :return: Square of Triangle
-#
-    #    Square of Triangle with coordinates:
-    #    (x1, y1), (x2, y2), (x3, y3)  can be counted by:
-    #    S = (x1y2 + x2y3 + x3y1 – x1y3 – x2y1 – x3y2)/2
-#
-    #    I use abs because on graph 'S' can be <= 0
-    #    """
-    #    for
-    #        return abs((self.
-    #def coordinates_increased(self, increasing_index: int):
-    #    """
-#
-    #    :param increasing_index: - Is the index that represents the number of coordinates increasing
-    #    :return: Increased coordinates by 'Increasing_index'
-    #    if coordinates increased by 'x', then 'S' - Square increases by '3' * 'x'
-    #    """
-    #    self.point_a_1 *= increasing_index
-    #    self.point_a_2 *= increasing_index
-    #    self.point_b_1 *= increasing_index
-    #    self.point_b_2 *= increasing_index
-    #    self.point_c_1 *= increasing_index
-    #    self.point_c_2 *= increasing_index
+        I use abs because on graph 'S' can be <= 0
+
+        Square of Triangle with coordinates:
+        (x1, y1), (x2, y2), (x3, y3)  can be counted by:
+        S = (x1y2 + x2y3 + x3y1 – x1y3 – x2y1 – x3y2)/2
+        """
+        for self.coordinates in self.name:                                                                                                                      #(x1y2 + x2y3 + x3y1 – x1y3 – x2y1 – x3y2)/2
+            return abs((self.point_1.coordinate_1 * self.point_2.coordinate_2
+                    + self.point_2.coordinate_1 * self.point_3.coordinate_2
+                    + self.point_3.coordinate_1 * self.point_1.coordinate_2
+                    - self.point_1.coordinate_1 * self.point_3.coordinate_2
+                    - self.point_2.coordinate_1 * self.point_1.coordinate_2
+                    - self.point_3.coordinate_1 * self.point_2.coordinate_2)/2)
+
+    def coordinates_increased(self, increasing_index: int):
+        """
+        :param increasing_index: number of increasing every coordinate
+        :return: Triangle with the same name but with changed coordinates, increased or decreased by 'increasing_index'
+        if coordinates increased by 'x', then 'S' - Square increases by '3' * 'x'
+        """
+        self.point_1.coordinate_1 *= increasing_index
+        self.point_1.coordinate_2 *= increasing_index
+        self.point_2.coordinate_1 *= increasing_index
+        self.point_2.coordinate_2 *= increasing_index
+        self.point_3.coordinate_1 *= increasing_index
+        self.point_3.coordinate_2 *= increasing_index
+        return Triangle
+
 
 A = Point(3, 7)
 B = Point(-2, 9)
 C = Point(-4, 22)
 ABC = Triangle("ABC", A, B, C)
+ABC.square_of_triangle()
+print(ABC.square_of_triangle())
 ABC.display()
+ABC.coordinates_increased(2)
+ABC.display()
+print(ABC.square_of_triangle())
 
 
-#XYZ.coordinates_increased(3)
-#XYZ.display()
-#print(XYZ.square_of_triangle())
