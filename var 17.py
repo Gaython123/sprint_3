70ead537c0c4c0ace96699ab653b17fe0f87e73c
lekorda <lesik2709@gmail.com>
2024-10-19 01:09:00 +0200

    docs added

--- var 17.py
+++ var 17.py
@@ -7,9 +7,15 @@ class Time:
         return f"Time - {self.hours}:{self.minutes}"
 
     def display(self):
+        """
+        :return: displays time
+        """
         print(f"Time - {self.hours}:{self.minutes}")
 
     def minutes_remaining(self):
+        """
+        :return: minutes to the enf of a day
+        """
         print(f"{60*24 - self.hours * 60 - self.minutes} minutes till day ends ")
 
 new_time = Time(12, 30)
