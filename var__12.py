cf0326660cc39313e9bb1ae32b82bbcdb22872cd
lekorda <lesik2709@gmail.com>
2024-10-18 23:36:12 +0200

    сюда чіназес готово
    added docstrings, finding fastest avg speed and best runner, structured the code

--- var__12.py
+++ var__12.py
@@ -7,9 +7,16 @@ class Time:
         return f"time is {self.minutes} minutes {self.seconds} seconds"
 
     def display_time(self):
+        """
+        :return: display the time of a run
+        """
         print(f"Time is '{self.minutes} min. {self.seconds} sec.'")
 
     def time_in_seconds(self):
+        """
+        :return: transform time into seconds
+        (make it easier to count meters per second (m/s)-> km/h
+        """
         return f"{self.minutes * 60 + self.seconds} second"
 
 class Run(Time):
@@ -23,29 +30,49 @@ class Run(Time):
                 f"in {self.minutes} minutes, {self.seconds} seconds'")
 
     def speed_avg(self):
+        """
+        :return: average speed in km/h
+        """
         for distance in self.runner_surname:
             avg_speed = self.distance / (self.minutes * 60 + self.seconds)
             avg_speed *= 3.6
             return round(avg_speed, 2)
 
     def speed_difference(self, new_run):
+        """
+        :param new_run: you use this method for one runner,
+        and you use 'new_run' to a runner you want to compare with
+        :return: difference in speed of two runners
+        """
         delta_speed = self.speed_avg() - new_run.speed_avg()
         return delta_speed
 
 class Leaderboard:
     def __init__(self):
+        """
+        list with all runner and their runs
+        """
         self.sportsmen = []
 
     def __str__(self):
         return f"{self.sportsmen}"
 
     def add_sportsman(self, run:Run):
+        """
+        :param run: a run (mainly runner_surname) you want to add to the list
+        :return: updating the list with new run
+        """
         if run in self.sportsmen:
             return
 
         self.sportsmen.append(run)
 
     def best_runner(self, n:int):
+        """
+        :param n: number of runs (runners and their average speed)
+        that you want to display
+        :return: list with the fastest runners in descending order
+        """
         for i in range(len(self.sportsmen)):
             for j in range(len(self.sportsmen)):
                 if self.sportsmen[i].speed_avg() > self.sportsmen[j].speed_avg():
