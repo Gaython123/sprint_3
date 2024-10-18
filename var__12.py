387a7e757dd58df21d8f1048ce7461ab656868e8
lekorda <lesik2709@gmail.com>
2024-10-18 23:21:18 +0200

    done(without docs

--- var__12.py
+++ var__12.py
@@ -26,12 +26,11 @@ class Run(Time):
         for distance in self.runner_surname:
             avg_speed = self.distance / (self.minutes * 60 + self.seconds)
             avg_speed *= 3.6
-            return avg_speed
+            return round(avg_speed, 2)
 
     def speed_difference(self, new_run):
         delta_speed = self.speed_avg() - new_run.speed_avg()
         return delta_speed
-
 
 class Leaderboard:
     def __init__(self):
@@ -44,29 +43,40 @@ class Leaderboard:
         if run in self.sportsmen:
             return
 
-        self.sportsmen.append(run.speed_avg())
+        self.sportsmen.append(run)
 
-    def best_runner(self):
+    def best_runner(self, n:int):
         for i in range(len(self.sportsmen)):
             for j in range(len(self.sportsmen)):
-                if self.sportsmen[i] > self.sportsmen[j]:
+                if self.sportsmen[i].speed_avg() > self.sportsmen[j].speed_avg():
                     self.sportsmen[i], self.sportsmen[j] = self.sportsmen[j], self.sportsmen[i]
 
-        #for avg_speed in self.sportsmen:
-        #    return run.runner_surname
-        return self.sportsmen[0], self.sportsmen[1]
+        best_runners = []
+        for run in self.sportsmen[:n]:
+            best_runners.append(f"{run.runner_surname} : {run.speed_avg()}")
+            #якщо ми напишемо ретурн з табуляцією то він поверне тільки 1 спортсмена
+            # (тобто лише один раз виконає цикл) а якщо без табуляції то допоки
+            # не пройде всю дистанцію (тобто 'n')
+        return best_runners
 
 
 Nazar_2004 = Run(3, 20, "Prots", 800)
-print(Nazar_2004.speed_avg())
-
 Ivan_2007 = Run(4, 40, "mykolenko",1000)
-print(Nazar_2004.speed_difference(Ivan_2007))
+Olena_2006 = Run(5, 40, "mykolencik",1000)
+Alex_2003 = Run(4, 20, "mykolenkavsky",1000)
 
 runner_list = Leaderboard()
+
 runner_list.add_sportsman(Ivan_2007)
 runner_list.add_sportsman(Nazar_2004)
-print(runner_list.best_runner())
+runner_list.add_sportsman(Olena_2006)
+runner_list.add_sportsman(Alex_2003)
+
+print(runner_list.best_runner(3))
+
+
+
+
 
 
 
