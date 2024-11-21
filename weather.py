4935cb1ba47266ce13c96fec78f66d0362716535
lekorda <lesik2709@gmail.com>
2024-11-21 13:53:05 +0100

    створено метод для отримання даних  з заданої колонки файлу csv

--- weather.py
+++ weather.py
@@ -1,36 +1,47 @@
 import csv
 from matplotlib import pyplot as plt
 
-with open('weather.csv') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
     #for row in csv_file:
     #    print(row)
-
-
     #row = next(csv_reader)
     #print(row['Data.Temperature.Avg Temp'].split(';'))
 
-class Weather:
-    def __init__(self, station_city: str, station_code: str, date: int,  temp_avg: float, temp_max: float,
-                 temp_min: float, wind_dir: float, wind_speed: float):
-
+class City:
+    def __init__(self, station_city: str, station_code: str):
         self.station_city = station_city
         self.station_code = station_code
-        self.date = date
-        self.temp_avg = temp_avg
-        self.temp_max = temp_max
-        self.temp_min = temp_min
-        self.wind_dir = wind_dir
-        self.wind_speed = wind_speed
+        self.date = []
+        self.temp_avg = []
+        self.temp_max = []
+        self.temp_min = []
+        self.wind_dir = []
+        self.wind_speed = []
 
     def __str__(self):
-        return (f"{self.station_city}, [{self.station_code}] in year {self.year}, "
+        return (f"{self.station_city}, [{self.station_code}]  "
                 f"had this statistics:\n"
                 f"Average temperature - {self.temp_avg}; "
                 f"Max temp - {self.temp_max}; "
                 f"Min Temp - {self.temp_min}; "
                 f"Wind direction - {self.wind_dir}; "
                 f"Wind speed - {self.wind_speed}; ")
+
+    def get_data(self, column):
+        self.data = []
+        with open('weather.csv', mode ='r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            for row in csv_reader:
+
+                #'Station.City мусимо вказувати як у csv file дослівно'
+                if row['Station.City'] == self.station_city:
+                    self.data.append(row[column])
+
+        return self.data
+
+
+city_one = City('Birmingham', 'BHM')
+print(city_one.get_data('Data.Temperature.Avg Temp'))
+
 
 
 
