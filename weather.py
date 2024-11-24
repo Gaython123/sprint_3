ae22a3401a7102368caffa03f20e8b5a013fa684
lekorda <lesik2709@gmail.com>
2024-11-24 17:46:55 +0100

    Revert "pizdec ja hujni narobyv"
    This reverts commit 013386a3a1599db0056c0612dea4dacf4a0cdf91.

--- weather.py
+++ weather.py
@@ -1,7 +1,5 @@
 import csv
 from datetime import datetime
-
-from fontTools.misc.cython import returns
 from matplotlib import pyplot as plt
 
     #for row in csv_file:
@@ -38,54 +36,16 @@ class City:
                 if row['Station.City'] == self.station_city:
                     row_date = datetime.strptime(row['Date.Full'], '%Y-%m-%d')
                     if row_date.year == year and row_date.month == month:
-                        self.data.append((row_date, row[column]))
-
+                        self.data.append(row[column])
 
         return self.data
 
-def bubble_sort(lst):
-    for i in range(len(lst)):
-        for j in range(len(lst)):
-            if lst[i] < lst[j]:
-                lst[i], lst[j] = lst[j], lst[i]
-
-    return lst
 
 
 city_one = City('Birmingham', 'BHM')
-data = city_one.get_data('Data.Temperature.Avg Temp', 2016, 1)
+print(city_one.get_data('Data.Temperature.Avg Temp', 2016, 1))
 
-#x_values = []
-#y_values = []
-#for item in data:
-#    x_value = item[0]
-#    y_value = item[1]
-#    x_values.append(x_value)
-#    y_value.append(x_value)
-#    return x_values, y_values
-
-
-print(data)
-#x_values = city_one.get_data('Date.Full', 2016, 1)
-#y_values = city_one.get_data('Data.Temperature.Avg Temp', 2016, 1)
-
-
-#sorted_data = bubble_sort(data)
-#x_values = [item[0] for item in sorted_data]
-#y_values = [item[1] for item in sorted_data]
-x_values = []
-y_values = []
-i=0
-for elem in range(len(data)):
-    x_values.append(data[0])
-    y_values.append(data[1])
-    i +=1
-
-
-plt.style.use('ggplot')
-plt.plot(x_values, y_values)
-plt.show()
-
-
+x_values = [1, 2, 3, 4, 5]
+y_values = city_one.get_data('Data.Temperature.Avg Temp', 2016, 1)
 
 
