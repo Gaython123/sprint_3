ed93d63ccb0d1a2c81f064275e97d80e0066362a
lekorda <lesik2709@gmail.com>
2024-11-27 14:43:10 +0100

    improved previous parts

--- weather.py
+++ weather.py
@@ -1,13 +1,7 @@
 import csv
 from datetime import datetime
-from types import new_class
 
 from matplotlib import pyplot as plt
-
-    #for row in csv_file:
-    #    print(row)
-    #row = next(csv_reader)
-    #print(row['Data.Temperature.Avg Temp'].split(';'))
 
 class City:
     def __init__(self, station_city: str, station_code: str):
@@ -28,29 +22,6 @@ class City:
                 f"Min Temp - {self.temp_min}; "
                 f"Wind direction - {self.wind_dir}; "
                 f"Wind speed - {self.wind_speed}; ")
-
-    #def get_data(self, column, year, month):
-    #    self.data = []
-    #    self.date = []
-    #    with open('weather.csv', mode ='r') as csv_file:
-    #        csv_reader = csv.DictReader(csv_file)
-    #        for row in csv_reader:
-    #            #'Station.City мусимо вказувати як у csv file дослівно'
-    #            if row['Station.City'] == self.station_city:
-    #                row_date = datetime.strptime(row['Date.Full'], '%Y-%m-%d')
-    #                if row_date.year == year and row_date.month == month:
-    #                    self.data.append(row[column])
-    #                    self.data = [*map(float, self.data)] #map - це розпакоУка елементів та надання їм типу даних
-    #                    #це тіпа recursion чи шо я єбу
-#
-#
-    #                    new_city = City('Birmingham', 'BMH')
-    #                    self.date.append(new_city.get_data('Date.Full', year, month))
-#
-    #        plt.plot(self.date, self.data)
-    #        plt.show()
-#
-    #    return self.data
 
     def get_data_from_column(self, column, year, month):
         self.data = []
@@ -79,22 +50,32 @@ class City:
 
         return self.date
 
-    def create_plot(self, column, year, month):
+    def create_plot_show(self, column: str, year: int, month: int, color_line_marker: str, linewidth_value: float, label_legend):
+        """
+        :param column: column to take data from
+        :param year: range for data
+        :param month: range for data
+        :param color_line_marker: stands for 'color', 'line' and 'marker' styles
+        :param linewidth_value: the width of line
+        :param label_legend: legend
+        :return: creates and returns a graph
+        """
         date_values_x = self.get_date(year, month)
         statistics_values_y = self.get_data_from_column(column, year, month)
 
-        plt.plot(date_values_x, statistics_values_y)
+        plt.plot(date_values_x, statistics_values_y, color_line_marker, linewidth = linewidth_value, label = label_legend)
+        plt.grid(True)
+
         plt.xlabel('Date')
         plt.ylabel(column)
-        plt.title(f'{column} in {self.station_city}, #{self.station_code} for {year}/{month}')
+        plt.title(f'{column} in {self.station_city}, ({self.station_code}) for {year}/{month}')
 
+        plt.legend()
         plt.show()
 
 city_one = City('Birmingham', 'BHM')
 
-#city_one.create_plot('Data.Temperature.Avg Temp', 2016, 1)
-
 city_two = City('Dillon', 'DLN')
-city_two.create_plot('Data.Wind.Speed', 2016, 4)
+city_two.create_plot_show('Data.Wind.Speed', 2016, 1, 'y-.', 2, 'Dillon wind speed')
 
 
